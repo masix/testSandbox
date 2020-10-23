f075c82a17df2c17b7cbcc4a6a4056a94c888994
Omar Ayoub <omar@abstract.ma>
2020-10-23 14:24:24 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
